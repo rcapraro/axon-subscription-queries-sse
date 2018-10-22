0de34a9029dc9521a03ec6c4b61ce9ff0c17ec1c
Troy Hart <troy.hart@gmail.com>
2018-10-22 00:46:34 -0600

    CLEANUP/BUG FIX
    Added query by example implementation and replaced a custom repository method with the QBE.
    
    Cleaned up dependencies.
    
    Cleaned up the front end a little.
    
    Fixed basket views by type query.

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,63 +1,56 @@
-var eventSource = null;
-var currentBasketId = null;
+var eventSource = undefined;
+var currentBasketId = undefined;
 
-function createBasket(basketType) {
+function createBasket() {
   $.ajax({
     url : `/api/baskets`,
     type : 'POST',
     success : function(data) {
       console.log("Created basket:......................", data);
-      showBasketDetail(data);
+      $("#subscribe-to-basket-id").val(data);
+      $(".lastbasketid").text(data);
+      subscribeToBasketModel();
     },
     data : JSON.stringify({
-      'type' : basketType
+      'type' : $("#basket-type").val()
     }),
     contentType : "application/json"
   });
 }
 
-function showBasketDetail(basketId) {
-  $(".row.basketdetail").show();
-  console.log('Showing basket details............................', basketId);
-  if (!currentBasketId) {
-    currentBasketId = basketId;
-    subscribe(currentBasketId);
-  } else if (basketId != currentBasketId) {
-    unsubscribe();
-    currentBasketId = basketId;
-    subscribe(currentBasketId);
+function subscribeToBasketModel() {
+  if (eventSource) {
+    unsubscribeFromBasketModel();
   }
-  $("#currentbasketid").text(currentBasketId);
-}
-
-function subscribe(basketId) {
-  eventSource = new EventSourcePolyfill(`/api/baskets/${basketId}`, {
-    headers : {
-      authorization: 'bearer my.token.value'
-    }
-  });
+  currentBasketId = $("#subscribe-to-basket-id").val();
+  $(".currentbasketid").text(currentBasketId);
+  eventSource = new EventSourcePolyfill(
+      `/api/baskets/${currentBasketId}/updates`, {
+        headers : {
+          authorization : 'bearer my.token.value'
+        }
+      });
   var listener = function(event) {
     $("#updatelog")
         .prepend(
             `<div>${event.type === "message" ? event.data : eventSource.url}</div>`)
-        .prepend(`<h3>${event.type}</h3>`);
+        .prepend(`<h4>${event.type}</h4>`);
   };
   eventSource.addEventListener("open", listener);
   eventSource.addEventListener("message", listener);
   eventSource.addEventListener("error", listener);
-  $(".row.basketdetail").show();
 }
 
-function unsubscribe() {
-  $(".row.basketdetail").hide();
+function unsubscribeFromBasketModel() {
   eventSource.close();
   eventSource = undefined;
 }
 
 function addThing() {
+
   console.log("Adding thing:......................", $("#thing-name").val(), $(
       "#thing-description").val());
-  
+
   $.ajax({
     url : `/api/baskets/${currentBasketId}/things`,
     type : 'PUT',
@@ -73,17 +66,24 @@ function addThing() {
 }
 
 $(function() {
-  $(".row.basketdetail").hide();
+
+  $(".baskettypelisting").hide();
+
   $("form").on('submit', function(e) {
     e.preventDefault();
   });
   $("#create-basket").click(function() {
-    createBasket($("#abstract-value").val());
+    createBasket();
   });
   $("#add-thing").click(function() {
     addThing();
   });
-  $("#subscribe-to-basket").click(function(event) {
-    showBasketDetail($("#abstract-value").val());
+  $("#subscribe-to-basket").click(function() {
+    subscribeToBasketModel();
+  });
+  $("#subscribe-to-basket-list-by-type").click(function(event) {
+    console.log("subscribe-to-basket-list-by-type clicked! ", event);
+    // $("#subscribe-to-basket-id").val();
+    // subscribeToBasketModel();
   });
 });
